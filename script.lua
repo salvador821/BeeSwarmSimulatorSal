8905fc1c623135fad5a9ef2b3d6e71da536d5501
salvador821 <losc30595@gmail.com>
2025-09-16 10:51:36 -0700

    Update script.lua
    Testts

--- script.lua
+++ script.lua
@@ -1,64 +1,43 @@
--- Load Orion Library
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+-- Load Rayfield Library
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Create Orion Window
-local Window = OrionLib:MakeWindow({
-    Name = "Speed Control",
-    HidePremium = false,
-    SaveConfig = false,
-    IntroEnabled = true,
-    IntroText = "WalkSpeed Controller"
+-- Create Window
+local Window = Rayfield:CreateWindow({
+   Name = "Speed Control",
+   LoadingTitle = "WalkSpeed Controller",
+   LoadingSubtitle = "Ready to use",
+   ConfigurationSaving = {
+      Enabled = false,
+   },
+   Discord = {
+      Enabled = false,
+   }
 })
 
--- Create a tab
-local Tab = Window:MakeTab({
-    Name = "Main",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
+-- Create Tab
+local MainTab = Window:CreateTab("Main", 4483361688)
+
+-- Create Button Section
+local ButtonSection = MainTab:CreateSection("WalkSpeed Controls")
+
+-- Create the button
+MainTab:CreateButton({
+   Name = "Set WalkSpeed to 60",
+   Callback = function()
+      local player = game.Players.LocalPlayer
+      local character = player.Character or player.CharacterAdded:Wait()
+      local humanoid = character:WaitForChild("Humanoid")
+      
+      humanoid.WalkSpeed = 60
+      
+      Rayfield:Notify({
+         Title = "Success!",
+         Content = "WalkSpeed set to 60",
+         Duration = 3,
+         Image = 4483361688
+      })
+   end,
 })
 
--- Show notification when UI loads
-OrionLib:MakeNotification({
-    Name = "UI Loaded!",
-    Content = "WalkSpeed controller is ready to use.",
-    Image = "rbxassetid://4483345998",
-    Time = 5
-})
-
--- Add button to change walkspeed
-Tab:AddButton({
-    Name = "Set WalkSpeed to 60",
-    Callback = function()
-        -- Get player character
-        local player = game.Players.LocalPlayer
-        if player.Character then
-            local humanoid = player.Character:FindFirstChild("Humanoid")
-            if humanoid then
-                humanoid.WalkSpeed = 60
-                OrionLib:MakeNotification({
-                    Name = "Success!",
-                    Content = "Your WalkSpeed has been set to 60.",
-                    Time = 3
-                })
-            else
-                OrionLib:MakeNotification({
-                    Name = "Error",
-                    Content = "No Humanoid found in your character.",
-                    Time = 3
-                })
-            end
-        else
-            OrionLib:MakeNotification({
-                Name = "Error",
-                Content = "No character found. Please wait for your character to load.",
-                Time = 3
-            })
-        end
-    end    
-})
-
--- Add a label for instructions
-Tab:AddLabel("Click the button to set your WalkSpeed to 60")
-
--- Initialize Orion UI
-OrionLib:Init()
+-- Load the UI
+Rayfield:LoadConfiguration()
